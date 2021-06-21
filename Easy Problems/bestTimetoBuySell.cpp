2b11b2fafa8094f1df45182a0e97750f7aea4732
yigittin <63165076+yigittin@users.noreply.github.com>
2021-06-22 00:56:48 +0300

    Add files via upload

---
+++ Easy Problems/bestTimetoBuySell.cpp
@@ -0,0 +1,15 @@
+//Valley-Peak
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int profit=0;
+        for(int i=0;i<n-1;i++){
+            if(prices[i]<prices[i+1]){
+                profit=profit+(prices[i+1]-prices[i]);
+            }            
+        }
+        return profit;
+    }
+};
